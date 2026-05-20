37aa571c5f33af395edee482e419dba6d9ae4de7
Ryodgie Barnatia <ryodgie@gmail.com>
2026-05-20 11:32:14 +0800

    chore: init astro project with scss and fontsource

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://ryodgie.com",
+  output: "static",
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "src/styles/tokens" as *;`,
+        },
+      },
+    },
+  },
+});
